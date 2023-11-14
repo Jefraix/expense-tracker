078662c35cb2d30a9e00678110707e0bc09bba66
juanaguileraht <JAguilera@heat-timer.com>
2023-11-14 10:53:17 -0500

    Lesson 141

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -16,15 +16,15 @@ const ManageExpense = ({ route, navigation }) => {
   }, [navigation, isEditing])
 
   const deleteExpenseHandler = () =>{
-
+    navigation.goBack();
   }
 
   const cancelHandler = () => {
-
+    navigation.goBack();
   }
 
   const confirmHandler = () => {
-
+    navigation.goBack();
   }
 
   return (
