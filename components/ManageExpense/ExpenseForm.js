5bb83f3e0ccb316372f29eeacbca927b141400e6
juanaguileraht <JAguilera@heat-timer.com>
2023-11-14 15:42:25 -0500

    Lesson 155

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -3,12 +3,13 @@ import Input from "./Input";
 import { useState } from "react";
 
 import Button from "../UI/Button";
+import { getFormattedDate } from "../../util/date";
 
-const ExpenseForm = ({ submitButtonLabel, onCancel, onSubmit }) => {
+const ExpenseForm = ({ submitButtonLabel, onCancel, onSubmit, defaultValues }) => {
   const [inputValues, setInputValues] = useState({
-    amount: "",
-    date: "",
-    description: "",
+    amount: defaultValues ? defaultValues.amount.toString() : "",
+    date: defaultValues ? getFormattedDate(defaultValues.date) : "",
+    description: defaultValues ? defaultValues.description : "",
   });
 
   const inputChangedHandler = (inputIdentifier, enteredValue) => {
