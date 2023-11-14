0452fa5341f8e18ec40512f60e04ec978e19eb64
juanaguileraht <JAguilera@heat-timer.com>
2023-11-14 12:07:30 -0500

    Lesson 142

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,39 +4,6 @@ import { GlobalStyles } from "../../constants/styles"
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19 00:00:00'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.99,
-    date: new Date('2022-01-05 00:00:00'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01 00:00:00'),
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19 00:00:00'),
-  },
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2022-02-18 00:00:00'),
-  },
-];
-
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return ( 
     <View style={styles.container}>
