078a6fb865d761568964512e245a0dcf9714a422
juanaguileraht <JAguilera@heat-timer.com>
2023-11-14 10:49:31 -0500

    Lesson 140

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from "../constants/styles";
+import Button from '../components/UI/Button';
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -18,9 +19,20 @@ const ManageExpense = ({ route, navigation }) => {
 
   }
 
+  const cancelHandler = () => {
+
+  }
+
+  const confirmHandler = () => {
+
+  }
+
   return (
     <View style={styles.container}>
-      
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>Cancel</Button>
+        <Button style={styles.button} onPress={confirmHandler}>{isEditing ? "Update" : "Add"}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton 
@@ -42,6 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
