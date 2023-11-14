576aef5b5b9102b8fe74bf518fa48aac05132b9c
juanaguileraht <JAguilera@heat-timer.com>
2023-11-14 09:55:29 -0500

    Lesson 135 starting point

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,9 +1,8 @@
 import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 const renderExpenseItem = (itemData) => {
-  return (
-    <Text>{itemData.item.description}</Text>
-  );
+  return <ExpenseItem {...itemData.item}/>;
 };
 
 const ExpensesList = ({ expenses }) => {
